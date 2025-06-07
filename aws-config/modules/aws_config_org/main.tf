1563e2e549ca336b53c6df12b0d4734820988a0c
Mahdi Bouaziz <bouazizmedmahdi@gmail.com>
2025-06-07 15:57:21 +0100

    add s3 bucket policies

--- aws-config/modules/aws_config_org/main.tf
+++ aws-config/modules/aws_config_org/main.tf
@@ -1,4 +1,43 @@
 ##########################
+# AWS S3 bucket policies
+##########################
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "config_logs_bucket_policy" {
+  bucket = var.config_logs_bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AWSConfigBucketPermissionsCheck",
+        Effect = "Allow",
+        Principal = {
+          Service = "config.amazonaws.com"
+        },
+        Action   = "s3:GetBucketAcl",
+        Resource = "arn:aws:s3:::${var.config_logs_bucket}"
+      },
+      {
+        Sid    = "AWSConfigBucketDelivery",
+        Effect = "Allow",
+        Principal = {
+          Service = "config.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${var.config_logs_bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/Config/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+##########################
 # AWS Config Recorder
 ##########################
 
